0827927c8eba417483c1e874413be98f85ab385b
Evan You <yyx990803@gmail.com>
2014-10-06 01:01:14 -0400

    bootstrap script (not tested at all)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,76 @@
-echo '. ~/.dotfiles/bash/.profile' > ~/.profile
+# make in case they aren't already there
+mkdir -p "/usr/local/lib"
+mkdir -p "/usr/local/bin"
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install and use latest bash
+brew install bash
+echo '/usr/local/bin/bash' > /etc/shells
+chsh -s /usr/local/bin/bash
+
+# Install git
+brew install git
+# Some git defaults
+git config --global color.ui true
+git config --global push.default simple
+
+# Install nvm
+echo "Installing nvm..."
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install stable
+nvm alias default stable
+
+# Centralize global npm packages for different node versions
+echo "prefix = /usr/local" > ~/.npmrc
+
+# Install Node modules
+modules=(
+  grunt-cli
+  jasmine-node
+  gulp
+  phantomjs
+  casperjs
+  anywhere
+  webpack
+  browserify
+  watchify
+  bower
+  starz
+)
+
+echo "installing node modules..."
+npm install -g ${modules[@]}
+
+# Install Brew Cask
+echo "Installing brew cask..."
+brew install caskroom/cask/brew-cask
+
+# Apps
+apps=(
+  google-chrome
+  firefox
+  iterm2
+  sublime-text3
+  divvy
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# clone this repo
+git clone https://github.com/yyx990803/dotfiles ~/.dotfiles
+
+# Source dot files
+echo '. ~/.dotfiles/bash/.profile' >> ~/.profile
 source ~/.profile
